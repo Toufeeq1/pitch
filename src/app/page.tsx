9713cdf091a0ee00dbd3660d9274c30125cefa63
Mogamat Toufeeq Farat <101368642+Toufeeq1@users.noreply.github.com>
2025-10-09 07:50:09 +0200

    UI/setup shadcn (#2)
    * setup shadcn/ui and add navbar component
    
    * made navbar component more responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold">PitchApp MVP 🚀</h1>
+       <div className="p-8">
+      <Button>Click me</Button>
+    </div>
     </main>
+    
   );
 }
